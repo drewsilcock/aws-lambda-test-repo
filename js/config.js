39441d1824df1c37593fd55e395dd5ac606b1ea0
Drew Silcock <drew.silcock@stfc.ac.uk>
2019-11-04 15:30:44 +0000

    Configure Cognito.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_9n950VcVy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6jd9kgtp82f4bhbv688dmqt4pj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
